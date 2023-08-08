4d5cd0b508b459680095b930df26823c011b3f04
emdiso <emdisomma@gmail.com>
2023-08-07 20:23:23 -0700

    fixed height

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,26 @@
 <script lang="ts">
   import imgSrc from "$lib/assets/image.png";
+  let heightRem: number = 0;
+  let innerWidth = 0;
+
+  $: {
+    if (innerWidth <= 480) {
+      heightRem = 15;
+    } else {
+      heightRem = 35;
+    }
+  }
 </script>
+
+<svelte:window bind:innerWidth />
 
 <div class="h-screen">
   <div class="flex flex-col h-full justify-center items-center">
-    <img style="--height:{35}rem" src={imgSrc} alt="Can't load right now..." />
+    <img
+      style="--height:{heightRem}rem"
+      src={imgSrc}
+      alt="Can't load right now..."
+    />
     <div class="flex justify-center p-1">
       <p class="hover:cursor-text font-bold text-[10px]">
         contact@outerbridgefilms.com
