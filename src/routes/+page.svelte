2771347c57172e6aff3898f70c00c62c1209a7a0
emdiso <emdisomma@gmail.com>
2023-08-07 20:07:35 -0700

    added img and text

---
+++ src/routes/+page.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  import imgSrc from "$lib/assets/image.png";
+</script>
+
+<div class="h-screen">
+  <div class="flex flex-col h-full justify-center items-center">
+    <img style="--height:{35}rem" src={imgSrc} alt="Can't load right now..." />
+    <div class="flex justify-center p-1">
+      <p class="hover:cursor-text font-bold text-[10px]">
+        contact@outerbridgefilms.com
+      </p>
+    </div>
+  </div>
+</div>
+
+<style lang="postcss">
+  img {
+    height: var(--height);
+  }
+  p {
+    font-family: Helvetica;
+  }
+</style>
